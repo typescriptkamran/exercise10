378a2d05b36c43680487dd86923a25d9852ddfce
kamran <kamran6333@gmail.com>
2023-08-01 00:42:57 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+/*
+
+My name: Muhammad kamran, Date: 1st Aug 2023
+This program is to Change the name cases into lower, upper and title case. 
+
+*/
+let personName: string = "muhaMMad kamraN ali rana"; 
+// this will change name to lower case
+let lowercaseName: string = personName.toLowerCase();
+
+// this will change it to upper case
+let uppercaseName: string = personName.toUpperCase();
+
+// this will split the name from spaces into array of and will store it into array name: words
+
+let words: string[] =personName.split(" ");
+
+// here taking an empty string
+let titleCaseName: string = "" 
+
+// using for loop to calling each word one by one from array words in order from 0 to end and chaning first chracter of each word in upper case and remaing in lower add adding them in empty string title case name one by one and space in between.
+
+for (let i = 0; i < words.length; i++) {
+     titleCaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " "
+    };
+// here is result
+
+console.log(lowercaseName);
+console.log(uppercaseName);
+console.log(titleCaseName);
+
+
+
